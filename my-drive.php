30f2a466c56a67c80035b04f5e953eac47d95591
luisberto99 <luisitomedina95@gmail.com>
2017-03-19 01:06:26 -0600

    Inico de la plantilla basica
    empece a hacer la cabecera de la plantilla basica

---
+++ my-drive.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mi Unidad - Boogle Drive</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/personalizado.css">
+</head>
+<body>
+	<div id="General" class="container-fluid">
+		<div class="row">
+			<div class="col-md-2 col-sm-2">
+				<a href="my-drive.php" value="Boogle Drive" class="textos"><label class="titulo">Boogle Drive</label></a>
+			</div>
+			<div class="col-md-7">
+				<form class="buscador">
+					<div class="btn-group" role="group">
+						<button type="button">
+							<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+						</button>
+						<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						    Action <span class="caret"></span>
+						  </button>
+						  <ul class="dropdown-menu">
+						    <li><a href="#">Action</a></li>
+						    <li><a href="#">Another action</a></li>
+						    <li><a href="#">Something else here</a></li>
+						    <li role="separator" class="divider"></li>
+						    <li><a href="#">Separated link</a></li>
+						  </ul>
+					</div>
+				</form>
+			</div>
+			<div class="col-md-2">
+				<div class="row">
+					<div class="col-md-4">
+						<input type="button" name="btn1" value="bu">
+					</div>
+					<div class="col-md-4">
+						<input type="button" name="btn1" value="bu">
+					</div>
+					<div class="col-md-4">
+						<input type="button" name="btn1" value="bu">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-2">
+				<input type="button" name="btn-Nuevo" value="Nuevo" class="btn btn-primary">
+			</div>
+			<div class="col-md-8">
+				<select>
+					<option>Mi unidad</option>
+					<option>Carpeta Nueva</option>
+				</select>
+			</div>
+			<div class="col-md-2">
+				<input type="button" name="btn1" value="bu">
+				<input type="button" name="btn2" value="bu">
+				<input type="button" name="btn3" value="bu">
+			</div>
+		</div>
+		<div class="row">
+
+			<div class="col-sm-4">
+				<div class="row">
+					<input type="button" name="btn-miUnidad" value="Mi Unidad">
+				</div>
+			</div>
+			<div class="col-sm-8">
+				
+			</div>
+		</div>
+	</div>
+
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.js"></script>
+</body>
+</html>
