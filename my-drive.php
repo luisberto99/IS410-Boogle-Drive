a4648f1c3c4caf57f685a550133279f26840e976
luisberto99 <luisitomedina95@gmail.com>
2017-03-20 00:52:46 -0600

    menus desplegables e iconos

--- my-drive.php
+++ my-drive.php
@@ -1,48 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Mi Unidad - Boogle Drive</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/personalizado.css">
-</head>
-<body>
-	<div id="General" class="container-fluid">
-		<div class="row cabecera">
-			<div class="col-md-2 col-sm-2 cabecera">
-				<a href="my-drive.php" value="Boogle Drive" class="textos"><label class="titulo">Boogle Drive</label></a>
-			</div>
-			<div class="col-md-6 cabecera">
-			<div class="row dropdown buscador">
-				<form id="buscador"">
-						<button class="transpariencia glyphicon glyphicon-search" aria-hidden="true"></button>
-						<input type="text" name="Buscar" id="txt-buscar" class=" transpariencia dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="buscador" placeholder="Buscar en drive">
-						<button class="transpariencia bm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="buscador" placeholder="Opciones de busqueda">
-							<span class="caret"></span>
-						</button>
-						<ul class="dropdown-menu menu_buscador" aria-lablelledby="buscador">
-							<li><a href="#">opci</a></li>
-							<li><a href="#">asdfa</a></li>
-						</ul>
-						</form>
-				</div>
-			</div>
-			<div class="col-md-1"></div>
-			<div class="col-md-2 cabecera">
-				<div class="row">
-					<button class="transpariencia btn_lateral" type="button"><span class="glyphicon glyphicon-bell" aria-hidden="true"></span></button>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-2">
-				<input type="button" name="btn-Nuevo" value="Nuevo" class="btn btn-primary">
-			</div>
-			<div class="col-md-8">
-				<div class="btn-group">
-					<button type="button" class=" btn btn-default btn-lg dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Mi unidad <span class="caret"></span>
-					</button>
-					<ul class="dropdown-menu">
+<?php
+	function menu_nuevo(){
+		echo '<ul class="dropdown-menu">
 						<li>
 							<a href="#">
 								<span class="glyphicon glyphicon-folder-open" aria-hiden="true">
@@ -82,13 +40,82 @@
 								Hoja de calculo de boogle
 							</a>
 						</li>
-					</ul>
+					</ul>';
+	}
+
+	function btn_superiores($icon){
+		echo '<button class="transpariencia btn_lateral dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" type="button"><span class="'.$icon.'" aria-hidden="true"></span></button>';
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mi Unidad - Boogle Drive</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/personalizado.css">
+</head>
+<body>
+	<div id="General" class="barra_superior">
+		<div class="row cabecera">
+			<div class="col-md-2 col-sm-2 cabecera">
+				<a href="my-drive.php" value="Boogle Drive" class="textos"><label class="titulo">Boogle Drive</label></a>
+			</div>
+			<div class="col-md-6 cabecera">
+				<div class="row dropdown buscador">
+					<form id="buscador"">
+						<button class="transpariencia glyphicon glyphicon-search" aria-hidden="true"></button>
+						<input type="text" name="Buscar" id="txt-buscar" class=" transpariencia dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="buscador" placeholder="Buscar en drive" style="width: 88%">
+						<button class="transpariencia bm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="buscador" placeholder="Opciones de busqueda">
+							<span class="caret"></span>
+						</button>
+						<ul class="dropdown-menu menu_buscador" aria-lablelledby="buscador">
+							<li><a href="#">Archivos PDF</a></li>
+							<li><a href="#">Documentos de texto</a></li>
+							<li><a href="#">Hojas de calculo</a></li>
+							<li><a href="#">Presentaciones</a></li>
+							<li><a href="#">Fotos e imagenes</a></li>
+							<li><a href="#">Videos</a></li>
+						</ul>
+					</form>
 				</div>
 			</div>
+			<div class="col-md-1"></div>
+			<div class="col-md-2 cabecera dere">
+				<div id="iconos1" class="row dere">
+				<div class="row col-md-2 dere">
+						<?php btn_superiores("glyphicon glyphicon-bell") ?>
+						<div class="dropdown-menu notificaciones menu_buscador">
+							Notificaciones
+						</div>
+					</div>
+					<div class="row col-md-2 dere">
+						<?php btn_superiores("glyphicon glyphicon-user") ?>
+						<div class="notificaciones dropdown-menu menu_buscador">
+								Perfil del usuario
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
 			<div class="col-md-2">
-				<input type="button" name="btn1" value="bu">
-				<input type="button" name="btn2" value="bu">
-				<input type="button" name="btn3" value="bu">
+				<button type="button" value="Nuevo" id="btn_nuevo" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-hapopup="true" aria-expanded="false">
+					Nuevo
+				</button>
+				<?php menu_nuevo() ?>
+			</div>
+			<div class="col-md-8">
+				<div class="btn-group">
+					<button type="button" class="transpariencia btn-lg dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Mi unidad <span class="caret"></span>
+					</button>
+					<?php menu_nuevo() ?>
+				</div>
+			</div>
+			<div class="col-md-2 dere">
+				<button class="transpariencia btn_lateral"><span class="glyphicon glyphicon-list" aria-hiddent=true></span></button>
+				<button class="transpariencia btn_lateral"><span class="glyphicon glyphicon-info-sign" aria-hiddet="true"></span></button>
+				<?php btn_superiores("glyphicon glyphicon-cog") ?>
 			</div>
 		</div>
 		<div class="row">
