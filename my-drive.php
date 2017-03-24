0b1642ecde1f8dce00ab1608e42c64e949543844
luisberto99 <luisitomedina95@gmail.com>
2017-03-24 12:50:02 -0600

    creacion de barra lateral

--- my-drive.php
+++ my-drive.php
@@ -1,51 +1,59 @@
 <?php
-	function menu_nuevo(){
-		echo '<ul class="dropdown-menu">
-						<li>
-							<a href="#">
-								<span class="glyphicon glyphicon-folder-open" aria-hiden="true">
-								</span>
-								Carpeta nueva...
-							</a>
-						</li>
-						<li role="separator" class="divider"></li>
-						<li>
-							<a href="#">
-								<span class="glyphicon glyphicon-folder-open" aria-hiden="true"></span>
-								Subir Archivos...
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								<span class="glyphicon glyphicon-folder-open" aria-hiden="true"></span>
-								Subir Archivos...
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								<span class="glyphicon glyphicon-folder-open" aria-hiden="true"></span>
-								Subir Carpeta...
-							</a>
-						</li>
-						<li role="separator" class="divider"></li>
-						<li>
-							<a href="#">
-								<span class="glyphicon glyphicon-folder-open" aria-hiden="true"></span>
-								Documento de boogle
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								<span class="glyphicon glyphicon-folder-open" aria-hiden="true"></span>
-								Hoja de calculo de boogle
-							</a>
-						</li>
-					</ul>';
-	}
+function menu_nuevo(){
+	echo '<ul class="dropdown-menu">
+	<li>
+		<a href="#">
+			<span class="glyphicon glyphicon-folder-open" aria-hiden="true">
+			</span>
+			Carpeta nueva...
+		</a>
+	</li>
+	<li role="separator" class="divider"></li>
+	<li>
+		<a href="#">
+			<span class="glyphicon glyphicon-folder-open" aria-hiden="true"></span>
+			Subir Archivos...
+		</a>
+	</li>
+	<li>
+		<a href="#">
+			<span class="glyphicon glyphicon-folder-open" aria-hiden="true"></span>
+			Subir Archivos...
+		</a>
+	</li>
+	<li>
+		<a href="#">
+			<span class="glyphicon glyphicon-folder-open" aria-hiden="true"></span>
+			Subir Carpeta...
+		</a>
+	</li>
+	<li role="separator" class="divider"></li>
+	<li>
+		<a href="#">
+			<span class="glyphicon glyphicon-folder-open" aria-hiden="true"></span>
+			Documento de boogle
+		</a>
+	</li>
+	<li>
+		<a href="#">
+			<span class="glyphicon glyphicon-folder-open" aria-hiden="true"></span>
+			Hoja de calculo de boogle
+		</a>
+	</li>
+</ul>';
+}
 
-	function btn_superiores($icon){
-		echo '<button class="transpariencia btn_lateral dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" type="button"><span class="'.$icon.'" aria-hidden="true"></span></button>';
-	}
+function btn_superiores($icon){
+	echo '<button class="transpariencia icono_lateral dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" type="button" ><span class="'.$icon.'" aria-hidden="true"></span></button>';
+}
+
+function btn_lateral($icono,$texto){
+	echo '<tr><td>
+	<button class="transpariencia btn_lateral">
+	<span class="'.$icono.' izq" aria-hidden="true" style="margin-right: 10px"></span>
+	<label class="izq">'.$texto.'</label>
+</button></td></tr>';
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -82,7 +90,7 @@
 			<div class="col-md-1"></div>
 			<div class="col-md-2 cabecera dere">
 				<div id="iconos1" class="row dere">
-				<div class="row col-md-2 dere">
+					<div class="row col-md-2 dere">
 						<?php btn_superiores("glyphicon glyphicon-bell") ?>
 						<div class="dropdown-menu notificaciones menu_buscador">
 							Notificaciones
@@ -91,7 +99,7 @@
 					<div class="row col-md-2 dere">
 						<?php btn_superiores("glyphicon glyphicon-user") ?>
 						<div class="notificaciones dropdown-menu menu_buscador">
-								Perfil del usuario
+							Perfil del usuario
 						</div>
 					</div>
 				</div>
@@ -113,20 +121,35 @@
 				</div>
 			</div>
 			<div class="col-md-2 dere">
-				<button class="transpariencia btn_lateral"><span class="glyphicon glyphicon-list" aria-hiddent=true></span></button>
-				<button class="transpariencia btn_lateral"><span class="glyphicon glyphicon-info-sign" aria-hiddet="true"></span></button>
+				<button class="transpariencia icono_lateral"><span class="glyphicon glyphicon-list" aria-hiddent=true></span></button>
+				<button class="transpariencia icono_lateral"><span class="glyphicon glyphicon-info-sign" aria-hiddet="true"></span></button>
 				<?php btn_superiores("glyphicon glyphicon-cog") ?>
 			</div>
 		</div>
-		<nav class="col-md-2 sidebar" >
-			<div class="col-md-10">
+		<div class="principal">
+			<nav class="col-md-2 sidebar" >
 				<div>
-					Mi unidad
+					<div>
+					<table class="table-hover">
+						<tr>
+							<td><button class="transpariencia btn_lateral">
+							<span class="glyphicon glyphicon-play izq" aria-hidden="true"></span>
+							<span class="glyphicon glyphicon-hdd izq" aria-hidden="true"></span>
+							<label class="izq">Mi unidad</label>
+						</button></td>
+						</tr>
+						<?php btn_lateral("glyphicon glyphicon-user","Compartido conmigo")	 ?>
+						<?php btn_lateral("glyphicon glyphicon-time","Reciente")	 ?>
+						<?php btn_lateral("glyphicon glyphicon-picture","Fotos")	 ?>
+						<?php btn_lateral("glyphicon glyphicon-star","Destacado")	 ?>
+						<?php btn_lateral("glyphicon glyphicon-trash","Papelera")	 ?>
+					</table>
+					</div>
 				</div>
-			</div>
-			<div class="col-md-2">
-			</div>
-		</nav>
+				<div class="col-md-2">
+				</div>
+			</nav>
+		</div>
 	</div>
 
 	<script src="js/jquery-latest.js"></script>
