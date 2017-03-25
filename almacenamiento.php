553ca4523de19c1d0bcbca1cb0742e379634b8d7
Zehuxx <juansoler631@gmail.com>
2017-03-24 20:37:25 -0600

    almacenamiento

---
+++ almacenamiento.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Almacenamiento</title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/personalizado.css">
+</head>
+<body>
+<div class="container-fluid">
+<div class="row">
+	
+
+<!-- As a link -->
+<div  id="encabezado" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+  <img id="boogle" src="img/google.png" width="92" height="36"></a>
+  <span id="bell" class="glyphicon glyphicon-bell" style="font-size: 20px; margin-right: 20px" ></span>
+  <span id="user" class="glyphicon glyphicon-user" style="font-size: 20px; margin-right: 45px " ></span>
+  
+</div>
+
+<div  id="subencabezado" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+  Almacenamiento de Drive
+
+</div>
+
+<div  id="middle" class="col-lg-12 col-md-4 col-sm-12 col-xs-12">
+<div id="grafico" class="col-lg-6 col-md-4">
+	
+</div>
+ <div class="col-lg-6 col-md-4 col-sm-12 col-xs-12">
+ <div id="myCarousel"  class="carousel slide" data-ride="carousel">
+      
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          
+          <div class="container">
+        
+              <h1>Example headline.</h1>
+              <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+            
+          </div>
+        </div>
+        <div class="item">
+          
+          <div class="container">
+            
+              <h1>Another example headline.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              
+          </div>
+        </div>
+        <div class="item">
+          
+          <div class="container">
+            
+              <h1>One more for good measure.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              
+            
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-controll" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-controll" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div><!-- /.carousel -->
+</div>
+<div class="col-xs-12 col-sm-12 col-md-4 col-lg-12 h1" >
+
+	<h1>Tu espacio de almacenamiento se comparte en</h1>
+
+<div class="col-lg-4">
+<h2 ><strong>Google Drive</strong></h2>
+<a  href="https://www.google.com/drive/" target="_blank" aria-label="Google Drive">
+<img src="img/drive.png" width="64" height="64">
+</a>
+<p >Almacena archivos de hasta 5 TB cada uno. Los documentos, hojas de cálculo o presentaciones de Google que crees no utilizarán el espacio de almacenamiento del que dispones.</p>
+</div>
+
+<div class="col-lg-4">
+<h2 ><strong>Gmail</strong></h2>
+<a  href="https://www.gmail.com/mail/help/about.html" target="_blank" aria-label="Gmail"><img src="img/gmail.png" width="64" height="64">
+</a>
+<p >Los archivos adjuntos enviados y recibidos en Gmail y tus mensajes de correo electrónico utilizan tu espacio de almacenamiento.</p>
+</div>
+
+<div class="col-lg-4">
+<h2><strong>Google Fotos</strong></h2>
+<a href="https://photos.google.com/?hl=es" target="_blank" aria-label="Google Fotos"><img src="img/fotos.png" width="64" height="64"></h2>
+</a>
+<p >Según tu configuración de Google Fotos, las subidas en alta calidad se almacenan de forma gratuita, pero las subidas en calidad original consumirán tu almacenamiento de Drive. 
+<a href="https://support.google.com/photos/answer/6314648?hl=es">Más información</a>
+</p>
+</div>
+
+</div>
+</div>
+<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
