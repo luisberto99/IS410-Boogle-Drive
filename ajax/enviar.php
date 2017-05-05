e7fc9f19b6ee84b718e61cd9df5999c6b39b4cf7
TaniaMGV <taniamgv93@hotmail.com>
2017-05-05 01:18:46 -0500

    mod

--- ajax/enviar.php
+++ ajax/enviar.php
@@ -1,7 +1,14 @@
 <?php 
 	
+	include_once("../class/class-conexion.php");
 	include "../conexion/funciones.php";
-	$quety = "INSERT INTO `mensajes` (`id`, `texto`) VALUES (NULL, '".$texto."');";
-	mysqli_query($con , $quety);
+	$conexion = new Conexion();
+	$conexion->establecerConexion();
+	$query = "INSERT INTO tbl_mensaje_chat(codigo_mensaje, codigo_usuario_envia, codigo_usuario_recibe, mensaje, fecha_mensaje, leido) VALUES (NULL,"",'".["codigo_usuario_envia"]."','"["codigo_usuario_recibe"]."','".$["mensaje"]."','"["fecha_mensaje"]."','".[""]);
+
+
+
+	$s = $conexion->ejecutarInstruccion($query);
 	
- ?>
+	
+ ?>​
--- ajax/mensajes.php
+++ ajax/mensajes.php
@@ -1,11 +1,18 @@
 <?php 
-	include "../conexion/funciones.php";
+	include_once("../class/class-conexion.php");
+	
+	$conexion = new conexion();
+	$conexion->establecerConexion();
+	$query="SELECT * FROM tbl_mensaje_chat WHERE 1 ";
 
-	$query = "SELECT * FROM mensajes ORDER BY id DESC LIMIT 10";
-	$s = mysqli_query($con, $query);
-	while ($r = mysqli_fetch_array($s)) {
+
+
+	
+	$s = $conexion->ejecutarInstruccion($query);
+	while ($r = $conexion->obtenerRegistro($s)) {
 		?>
-		mensaje: <?=$r['texto']?><br>
+		mensaje de: <?=$r["codigo_usuario_envia"]." ".$r['mensaje']?><br>
 		<?php
 	}
  ?>  
+
